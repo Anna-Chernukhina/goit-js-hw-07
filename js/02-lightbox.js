25e06f70e5b66f9e047acd74beb2b6b41f02cd00
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-12 11:47:13 +0100

    markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imagesList = document.querySelector(".gallery"); // ref for ul
+
+//* task 1. Разметка
+function createImagesListMarkup(images){
+    const markup = images.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                src="${preview}" 
+                alt="${description}" />
+            </a>
+        </li>`
+    }).join("");
+
+    return markup;
+   }
+
+imagesList.insertAdjacentHTML("beforeend", createImagesListMarkup(galleryItems));
